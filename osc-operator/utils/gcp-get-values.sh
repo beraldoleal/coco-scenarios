e7fbc908d8235be856fdb2c4c248ef061716ff2c
Beraldo Leal <bleal@redhat.com>
2025-03-13 12:35:42 -0400

    small improvements
    Signed-off-by: Beraldo Leal <bleal@redhat.com>

--- osc-operator/utils/gcp-get-values.sh
+++ osc-operator/utils/gcp-get-values.sh
@@ -8,4 +8,4 @@
 #  - GCP_NETWORK
 export GCP_PROJECT_ID=$(oc get infrastructure cluster -o json | jq -r '.status.platformStatus.gcp.projectID')
 export GCP_REGION=$(oc get infrastructure cluster -o json | jq -r '.status.platformStatus.gcp.region')
-export GCP_NETWORK=$(oc get machine -n openshift-machine-api -o jsonpath="{.items[?(@.status.nodeRef)].spec.providerSpec.value.networkInterfaces[0].network}" | head -n 1)
+export GCP_NETWORK=$(oc get machine -n openshift-machine-api -o jsonpath="{.items[?(@.metadata.name=='ci-ln-m4j0vlb-72292-95mrh-worker-a-f9fl6')].spec.providerSpec.value.networkInterfaces[0].network}")
