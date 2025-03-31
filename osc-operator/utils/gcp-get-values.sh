6a86bc736870cd79091f0505befab97e2e2b87aa
Beraldo Leal <bleal@redhat.com>
2025-03-31 12:05:46 -0400

    small fixes
    Signed-off-by: Beraldo Leal <bleal@redhat.com>

--- osc-operator/utils/gcp-get-values.sh
+++ osc-operator/utils/gcp-get-values.sh
@@ -6,6 +6,11 @@
 #  - GCP_PROJECT_ID
 #  - GCP_REGION
 #  - GCP_NETWORK
-export GCP_PROJECT_ID=$(oc get infrastructure cluster -o json | jq -r '.status.platformStatus.gcp.projectID')
-export GCP_REGION=$(oc get infrastructure cluster -o json | jq -r '.status.platformStatus.gcp.region')
-export GCP_NETWORK=$(oc get machine -n openshift-machine-api -o jsonpath="{.items[?(@.metadata.name=='ci-ln-m4j0vlb-72292-95mrh-worker-a-f9fl6')].spec.providerSpec.value.networkInterfaces[0].network}")
+
+WORKERNODE="$(oc get nodes | grep worker | head -1 | cut -d\   -f1)"
+#export GCP_PROJECT_ID=$(oc get infrastructure cluster -o json | jq -r '.status.platformStatus.gcp.projectID')
+export GCP_PROJECT_ID="it-cloud-gcp-prod-osc-devel"
+#export GCP_REGION=$(oc get infrastructure cluster -o json | jq -r '.status.platformStatus.gcp.region')
+export GCP_REGION="us-central1"
+export GCP_NETWORK=$(oc get machine -n openshift-machine-api -o jsonpath="{.items[?(@.metadata.name=='${WORKERNODE}')].spec.providerSpec.value.networkInterfaces[0].network}")
+# export GCP_NETWORK="default"
