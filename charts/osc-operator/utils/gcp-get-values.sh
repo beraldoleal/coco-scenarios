df5b721cdd084dcf443fd0597c3aec1a0617a289
Beraldo Leal <bleal@redhat.com>
2025-06-25 16:59:13 -0400

    refactoring
    Signed-off-by: Beraldo Leal <bleal@redhat.com>

--- charts/osc-operator/utils/gcp-get-values.sh
+++ charts/osc-operator/utils/gcp-get-values.sh
@@ -12,5 +12,7 @@ WORKERNODE="$(oc get nodes | grep worker | head -1 | cut -d\   -f1)"
 export GCP_PROJECT_ID="it-cloud-gcp-prod-osc-devel"
 #export GCP_REGION=$(oc get infrastructure cluster -o json | jq -r '.status.platformStatus.gcp.region')
 export GCP_REGION="us-central1"
-export GCP_NETWORK=$(oc get machine -n openshift-machine-api -o jsonpath="{.items[?(@.metadata.name=='${WORKERNODE}')].spec.providerSpec.value.networkInterfaces[0].network}")
-# export GCP_NETWORK="default"
+#export GCP_NETWORK=$(oc get machine -n openshift-machine-api -o jsonpath="{.items[?(@.metadata.name=='${WORKERNODE}')].spec.providerSpec.value.networkInterfaces[0].network}")
+export GCP_NETWORK="default"
+export GCP_MACHINE_TYPE="e2-medium" # or any other
+export PODVM_IMAGE_NAME="podvm-image-2025033151"
