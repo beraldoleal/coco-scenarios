845e20cac1a37c1c56a2fda46dbdcea923abecde
Beraldo Leal <bleal@redhat.com>
2025-07-15 14:53:00 -0400

    trustee: renaming to trustee-operands
    Signed-off-by: Beraldo Leal <bleal@redhat.com>

--- charts/trustee-operands/utils/trustee-gen-keypair.sh
+++ charts/trustee-operands/utils/trustee-gen-keypair.sh
@@ -11,6 +11,6 @@ openssl pkey -in "${OUT_DIR}/privateKey" -pubout -out "${OUT_DIR}/publicKey"
 base64 -w 0 ${OUT_DIR}/publicKey > ${OUT_DIR}/publicKey.b64
 
 echo "✅ Keys written to ${OUT_DIR}:"
-echo "   - privateKey"
-echo "   - publicKey"
-echo "   - publicKey.b64"
+echo "   - ${OUT_DIR}/privateKey"
+echo "   - ${OUT_DIR}/publicKey"
+echo "   - ${OUT_DIR}/publicKey.b64"
